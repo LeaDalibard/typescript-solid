5dc691612e1c8c2475073fd87ca22e9bfcacc8d4
Lea Dalibard <leadalibard@gmail.com>
2020-12-01 11:12:31 +0100

    2.L implementing interface Discount and passing discount type into variables

--- SOLID/2.L/old.js
+++ SOLID/2.L/old.js
@@ -1,8 +1,12 @@
 "use strict";
+//This is called a Union, the discountType can only contain the following 2 values:
+var VARIABLE = "variable";
+var FIXED = "fixed";
+var NO_DISCOUNT = "none";
 var VariableDiscount = /** @class */ (function () {
-    function VariableDiscount(type, value) {
+    function VariableDiscount(value) {
         if (value === void 0) { value = 0; }
-        this._type = "variable";
+        this._type = VARIABLE;
         this._value = value;
         if (value <= 0) {
             throw new Error('You cannot create a ' + this._type + ' discount with a negative value');
@@ -17,9 +21,9 @@ var VariableDiscount = /** @class */ (function () {
     return VariableDiscount;
 }());
 var FixedDiscount = /** @class */ (function () {
-    function FixedDiscount(type, value) {
+    function FixedDiscount(value) {
         if (value === void 0) { value = 0; }
-        this._type = "fixed";
+        this._type = FIXED;
         this._value = value;
         if (value <= 0) {
             throw new Error('You cannot create a ' + this._type + ' discount with a negative value');
@@ -34,13 +38,9 @@ var FixedDiscount = /** @class */ (function () {
     return FixedDiscount;
 }());
 var NoDiscount = /** @class */ (function () {
-    function NoDiscount(type, value) {
-        if (value === void 0) { value = 0; }
-        this._type = "none";
-        this._value = value;
-        if (value <= 0) {
-            throw new Error('You cannot create a ' + this._type + ' discount with a negative value');
-        }
+    function NoDiscount() {
+        this._type = NO_DISCOUNT;
+        this._value = 0;
     }
     NoDiscount.prototype.apply = function (price) {
         return price;
@@ -50,44 +50,6 @@ var NoDiscount = /** @class */ (function () {
     };
     return NoDiscount;
 }());
-/* class Discount {
-    private _type: discountType;
-    private _value: number;
-
-    constructor(type: discountType, value: number = 0) {
-        this._type = type;
-        this._value = value;
-
-        if (this._type != 'none' && value <= 0) {
-            throw new Error('You cannot create a ' + this._type + ' discount with a negative value');
-        }
-    }
-
-    apply(price: number): number {
-        //@todo: instead of using magic values as string in this, it would be a lot better to change them into constant. This would protect us from misspellings. Can you improve this?
-        if (this._type === "none") {
-            return price;
-        } else if (this._type === "variable") {
-            return (price - (price * this._value / 100));
-        } else if (this._type === "fixed") {
-            return Math.max(0, price - this._value);
-        } else {
-            throw new Error('Invalid type of discount');
-        }
-    }
-
-    showCalculation(price: number): string {
-        if (this._type === "none") {
-            return "No discount";
-        } else if (this._type === "variable") {
-            return price + " € -  " + this._value + "%";
-        } else if (this._type === "fixed") {
-            return price + "€ -  " + this._value + "€ (min 0 €)";
-        } else {
-            throw new Error('Invalid type of discount');
-        }
-    }
-}*/
 var Product = /** @class */ (function () {
     function Product(name, price, discount) {
         this._name = name;
@@ -143,10 +105,11 @@ var shoppingBasket = /** @class */ (function () {
     return shoppingBasket;
 }());
 var cart = new shoppingBasket();
-cart.addProduct(new Product('Chair', 25, new Discount("fixed", 10)));
+cart.addProduct(new Product('Chair', 25, new FixedDiscount(10)));
 //cart.addProduct(new Product('Chair', 25, new Discount("fixed", -10)));
-cart.addProduct(new Product('Table', 50, new Discount("variable", 25)));
-cart.addProduct(new Product('Bed', 100, new Discount("none")));
+cart.addProduct(new Product('Table', 50, new VariableDiscount(25)));
+//cart.addProduct(new Product('Table', 50, new Discount("variable", 25)));
+cart.addProduct(new Product('Bed', 100, new NoDiscount));
 var tableElement = document.querySelector('#cart tbody');
 cart.products.forEach(function (product) {
     var tr = document.createElement('tr');
@@ -164,3 +127,41 @@ cart.products.forEach(function (product) {
     tr.appendChild(td);
     tableElement.appendChild(tr);
 });
+/* class Discount {
+    private _type: discountType;
+    private _value: number;
+
+    constructor(type: discountType, value: number = 0) {
+        this._type = type;
+        this._value = value;
+
+        if (this._type != 'none' && value <= 0) {
+            throw new Error('You cannot create a ' + this._type + ' discount with a negative value');
+        }
+    }
+
+    apply(price: number): number {
+        //@todo: instead of using magic values as string in this, it would be a lot better to change them into constant. This would protect us from misspellings. Can you improve this?
+        if (this._type === "none") {
+            return price;
+        } else if (this._type === "variable") {
+            return (price - (price * this._value / 100));
+        } else if (this._type === "fixed") {
+            return Math.max(0, price - this._value);
+        } else {
+            throw new Error('Invalid type of discount');
+        }
+    }
+
+    showCalculation(price: number): string {
+        if (this._type === "none") {
+            return "No discount";
+        } else if (this._type === "variable") {
+            return price + " € -  " + this._value + "%";
+        } else if (this._type === "fixed") {
+            return price + "€ -  " + this._value + "€ (min 0 €)";
+        } else {
+            throw new Error('Invalid type of discount');
+        }
+    }
+}*/ 
--- SOLID/2.L/old.ts
+++ SOLID/2.L/old.ts
@@ -1,12 +1,15 @@
 //This is called a Union, the discountType can only contain the following 2 values:
-type discountType = "variable" | "fixed" | "none";
+const VARIABLE= "variable";
+const FIXED= "fixed";
+const NO_DISCOUNT= "none";
+type discountType = typeof VARIABLE | typeof FIXED | typeof NO_DISCOUNT;
+
+
 
 interface Discount {
-    type: discountType;
-    value: number;
-
+    _type: discountType;
+    _value: number;
     showCalculation(price: number): string;
-
     apply(price: number): number;
 }
 
@@ -15,8 +18,8 @@ class VariableDiscount implements Discount {
     private _type: discountType;
     private _value: number;
 
-    constructor(type: discountType, value: number = 0) {
-        this._type = "variable";
+    constructor(value: number = 0) {
+        this._type = VARIABLE;
         this._value = value;
         if (value <= 0) {
             throw new Error('You cannot create a ' + this._type + ' discount with a negative value');
@@ -38,8 +41,8 @@ class FixedDiscount implements Discount {
     private _type: discountType;
     private _value: number;
 
-    constructor(type: discountType, value: number = 0) {
-        this._type = "fixed";
+    constructor(value: number = 0) {
+        this._type = FIXED;
         this._value = value;
         if (value <= 0) {
             throw new Error('You cannot create a ' + this._type + ' discount with a negative value');
@@ -61,12 +64,9 @@ class NoDiscount implements Discount {
     private _type: discountType;
     private _value: number;
 
-    constructor(type: discountType, value: number = 0) {
-        this._type = "none";
-        this._value = value;
-        if (value <= 0) {
-            throw new Error('You cannot create a ' + this._type + ' discount with a negative value');
-        }
+    constructor() {
+        this._type =NO_DISCOUNT;
+        this._value = 0;
     }
 
     apply(price: number): number {
@@ -78,45 +78,6 @@ class NoDiscount implements Discount {
     }
 
 }
-
-/* class Discount {
-    private _type: discountType;
-    private _value: number;
-
-    constructor(type: discountType, value: number = 0) {
-        this._type = type;
-        this._value = value;
-
-        if (this._type != 'none' && value <= 0) {
-            throw new Error('You cannot create a ' + this._type + ' discount with a negative value');
-        }
-    }
-
-    apply(price: number): number {
-        //@todo: instead of using magic values as string in this, it would be a lot better to change them into constant. This would protect us from misspellings. Can you improve this?
-        if (this._type === "none") {
-            return price;
-        } else if (this._type === "variable") {
-            return (price - (price * this._value / 100));
-        } else if (this._type === "fixed") {
-            return Math.max(0, price - this._value);
-        } else {
-            throw new Error('Invalid type of discount');
-        }
-    }
-
-    showCalculation(price: number): string {
-        if (this._type === "none") {
-            return "No discount";
-        } else if (this._type === "variable") {
-            return price + " € -  " + this._value + "%";
-        } else if (this._type === "fixed") {
-            return price + "€ -  " + this._value + "€ (min 0 €)";
-        } else {
-            throw new Error('Invalid type of discount');
-        }
-    }
-}*/
 
 
 class Product {
@@ -167,10 +128,11 @@ class shoppingBasket {
 }
 
 let cart = new shoppingBasket();
-cart.addProduct(new Product('Chair', 25, new Discount("fixed", 10)));
+cart.addProduct(new Product('Chair', 25, new FixedDiscount(10)));
 //cart.addProduct(new Product('Chair', 25, new Discount("fixed", -10)));
-cart.addProduct(new Product('Table', 50, new Discount("variable", 25)));
-cart.addProduct(new Product('Bed', 100, new Discount("none")));
+cart.addProduct(new Product('Table', 50, new VariableDiscount(25)));
+//cart.addProduct(new Product('Table', 50, new Discount("variable", 25)));
+cart.addProduct(new Product('Bed', 100, new NoDiscount));
 
 const tableElement = document.querySelector('#cart tbody');
 cart.products.forEach((product) => {
@@ -194,3 +156,43 @@ cart.products.forEach((product) => {
 
     tableElement.appendChild(tr);
 });
+
+
+/* class Discount {
+    private _type: discountType;
+    private _value: number;
+
+    constructor(type: discountType, value: number = 0) {
+        this._type = type;
+        this._value = value;
+
+        if (this._type != 'none' && value <= 0) {
+            throw new Error('You cannot create a ' + this._type + ' discount with a negative value');
+        }
+    }
+
+    apply(price: number): number {
+        //@todo: instead of using magic values as string in this, it would be a lot better to change them into constant. This would protect us from misspellings. Can you improve this?
+        if (this._type === "none") {
+            return price;
+        } else if (this._type === "variable") {
+            return (price - (price * this._value / 100));
+        } else if (this._type === "fixed") {
+            return Math.max(0, price - this._value);
+        } else {
+            throw new Error('Invalid type of discount');
+        }
+    }
+
+    showCalculation(price: number): string {
+        if (this._type === "none") {
+            return "No discount";
+        } else if (this._type === "variable") {
+            return price + " € -  " + this._value + "%";
+        } else if (this._type === "fixed") {
+            return price + "€ -  " + this._value + "€ (min 0 €)";
+        } else {
+            throw new Error('Invalid type of discount');
+        }
+    }
+}*/
